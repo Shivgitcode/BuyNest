0e4cdf2d9137f80cb06a403a2fffc98c89336aee
Shivgitcode <shivneeraj2004@gmail.com>
2025-05-06 12:43:15 +0530

    deploying project now

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-	baseURL: `${import.meta.env.VITE_BASE_URL}`,
+	baseURL: `${import.meta.env.NODE_ENV === "production" ? import.meta.env.VITE_BASE_URL_HOSTED : import.meta.env.VITE_BASE_URL}`,
 	timeout: 100000,
 	withCredentials: true,
 });
